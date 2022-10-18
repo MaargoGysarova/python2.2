207a829318c7844342aab1bae55905b551640122
margogusarova <margogusarova19@gmail.com>
2022-10-18 22:44:20 +0400

    Добавлена функция получения массива путей файлов

--- csv file(path 1).py
+++ csv file(path 1).py
@@ -3,13 +3,22 @@ import os
 
 
 def write_into_csv(way_to_dataset, way_to_files):
-    with open("dataset_csv_first", 'w', encoding='utf-8', newline='') as file:
+    with open("dataset_csv_first", 'w+', encoding='utf-8', newline='') as file:
         file_writer = csv.writer(file, delimiter=';')
         file_writer.writerow(["Absolute way", "Relative way", "Class"])
 
         for i in range(0, len(way_to_files)):
-            file_writer.writerow([f'{way_to_dataset + way_to_files[i]}', f'..\\dataset{way_to_files[i]}', f'{way_to_files[i][1]}'])
+            file_writer.writerow(
+                [f'{way_to_dataset + way_to_files[i]}', f'..\\dataset{way_to_files[i]}', f'{way_to_files[i][1]}'])
 
 
+def get_way_to_files(way_to_dataset, name):
+    ways_to_files = list()
+    folder_way = way_to_dataset + '\\' + name
+    num_of_files = sorted(os.listdir(folder_way))  # кол-во файлов
 
+    for file_num in range(1, num_of_files):
+        way_to_file = folder_way + f'\\{file_num:04}' + '.jpg'
+        ways_to_files.append(way_to_file)
 
+    return ways_to_files
