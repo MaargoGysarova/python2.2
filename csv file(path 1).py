607e588f7330cd5cd822bc56c4569988fd9fe468
margogusarova <margogusarova19@gmail.com>
2022-10-20 11:07:19 +0400

    Доработаны функции.Получение csv файла по заданию

--- csv file(path 1).py
+++ csv file(path 1).py
@@ -8,17 +8,31 @@ def write_into_csv(way_to_dataset, way_to_files):
         file_writer.writerow(["Absolute way", "Relative way", "Class"])
 
         for i in range(0, len(way_to_files)):
+            way_to_file = f'dataset/{way_to_files[i]}'
             file_writer.writerow(
-                [f'{way_to_dataset + way_to_files[i]}', f'..\\dataset{way_to_files[i]}', f'{way_to_files[i][1]}'])
+                [f'{way_to_dataset + "/" + way_to_files[i]}', f'  /dataset/{way_to_files[i]}',
+                os.path.basename(os.path.dirname(way_to_file))])
 
 
 def get_way_to_files(way_to_dataset, name):
     ways_to_files = list()
-    folder_way = way_to_dataset + '\\' + name
-    num_of_files = sorted(os.listdir(folder_way))  # кол-во файлов
+    for i in range(0, len(name)):
+        # folder_way = way_to_dataset + '/' + name[i]
+        folder_way = way_to_dataset + '/' + name[i]
+        num_of_files = os.listdir(folder_way)  # кол-во файлов
 
-    for file_num in range(1, num_of_files):
-        way_to_file = folder_way + f'\\{file_num:04}' + '.jpg'
-        ways_to_files.append(way_to_file)
+        for file_num in range(1, len(num_of_files) + 1):
+            way_to_file = name[i] + f'/{file_num:04}' + '.jpg'
+            ways_to_files.append(way_to_file)
 
     return ways_to_files
+
+
+if __name__ == '__main__':
+    name = ['tiger', 'leopard']
+    way_to_dataset = os.path.abspath("../python2.1/dataset")
+    ways_to_files = get_way_to_files(way_to_dataset, name)
+
+    write_into_csv(way_to_dataset, ways_to_files)
+
+    print("Работа окончена")
