0c9f7d4c3d11241053a379e17abdcdc6e3530b6e
margogusarova <margogusarova19@gmail.com>
2022-10-22 21:15:37 +0400

    Работа над 2 частью л/р : 1)Доработана функция копирования в новую папку 2)Прописана фукнция создания csv файла

--- csv_file_first.py
+++ csv_file_first.py
@@ -16,21 +16,22 @@ def write_into_csv(way_to_dataset, way_to_files):
 
 def get_way_to_files(way_to_dataset, name):
     ways_to_files = list()
+    num_names = list()
     for i in range(0, len(name)):
         folder_way = way_to_dataset + '/' + name[i]
         num_of_files = os.listdir(folder_way)  # кол-во файлов
-
+        num_names.append(len(num_of_files))
         for file_num in range(1, len(num_of_files) + 1):
             way_to_file = name[i] + f'/{file_num:04}' + '.jpg'
             ways_to_files.append(way_to_file)
 
-    return ways_to_files
+    return [ways_to_files, num_names]
 
 
 if __name__ == '__main__':
     name = ['tiger', 'leopard']
     way_to_dataset = os.path.abspath("../python2.1/dataset")
-    ways_to_files = get_way_to_files(way_to_dataset, name)
+    ways_to_files = get_way_to_files(way_to_dataset, name)[0]
 
     write_into_csv(way_to_dataset, ways_to_files)
 
--- csv_file_second.py
+++ csv_file_second.py
@@ -4,27 +4,57 @@ import shutil
 from csv_file_first import get_way_to_files
 
 
-def write_into_csv(way_to_dataset, way_to_files):
-    with open("dataset_csv_first", 'w+', encoding='utf-8', newline='') as file:
+def write_into_csv(way_to_dataset, way_to_files, name):
+    names = list()
+    num_names = get_way_to_files(way_to_dataset, name)[1]
+    i = 0
+    with open("dataset_csv_second", 'w+', encoding='utf-8', newline='') as file:
         file_writer = csv.writer(file, delimiter=';')
         file_writer.writerow(["Absolute way", "Relative way", "Class"])
 
         for i in range(0, len(way_to_files)):
-            way_to_file = f'dataset/{way_to_files[i]}'
-            file_writer.writerow(
-                [f'{way_to_dataset + "/" + way_to_files[i]}', f'  /dataset/{way_to_files[i]}',
-                 os.path.basename(os.path.dirname(way_to_file))])
+            if i < num_names[0]:
+                way_to_file = f'dataset/{way_to_files[i]}'
+                file_writer.writerow(
+                    [f'{way_to_files[i]}', f'  dataset_all_names/{os.path.basename(way_to_files[i])}',
+                     name[0]])
+            else:
+                way_to_file = f'dataset/{way_to_files[i]}'
+                file_writer.writerow(
+                    [f'{way_to_files[i]}', f'  dataset_all_names/{os.path.basename(way_to_files[i])}',
+                     name[1]])
 
 
 def get_way_to_files2(way_to_dataset, name):
+    num_names = get_way_to_files(way_to_dataset, name)[1]
     ways_to_files = list()
-    ways_to_files = get_way_to_files(way_to_dataset, name)
-
-    i = 0
+    ways_to_files = get_way_to_files(way_to_dataset, name)[0]
+    ways_to_files_second = list()
+    i = 1
     for file in ways_to_files:
-        way_to_file = "dataset/dataset_allnames/f'{name}_{i:04}'+'.jpg'"
-        shutil.copy(file,way_to_file)
-        i += 1
+        file = "../python2.1/dataset" + "/" + file
+        if i < num_names[0]:
+            way_to_file = os.path.abspath("../python2.1") + "/" + f'dataset_all_names/{name[0]}_{i:04}.jpg'
+            ways_to_files_second.append(way_to_file)
+            shutil.copyfile(file, way_to_file)
+            i += 1
+        else:
+            way_to_file = os.path.abspath("../python2.1") + "/" + f'dataset_all_names/{name[1]}_{i:04}.jpg'
+            ways_to_files_second.append(way_to_file)
+            shutil.copyfile(file, way_to_file)
+            i += 1
+
+    return ways_to_files_second
 
 
+if __name__ == '__main__':
+    name = ['tiger', 'leopard']
+    if os.path.isdir('../python2.1/dataset_all_names') == 1:
+        shutil.rmtree('../python2.1/dataset_all_names')
+    os.mkdir('../python2.1/dataset_all_names')
+    way_to_dataset = os.path.abspath("../python2.1/dataset")
+    ways_to_files = get_way_to_files2(way_to_dataset, name)
 
+    write_into_csv(way_to_dataset, ways_to_files, name)
+
+    print("Работа окончена")
