0a9be8ec82452302a937749cdbb97fa7ce711ee4
margogusarova <margogusarova19@gmail.com>
2022-10-23 20:58:01 +0400

    Добавлен скрипт, содержащий функцию, получающую на входе метку класса и возвращающую следующий экземпляр (путь к нему) этого класса

---
+++ get_one_way.py
@@ -0,0 +1,20 @@
+import os
+import random
+
+def get_way(name, num_of_file , i , ):
+    if i==
+    way_to_dataset = os.path.abspath("../python2.1/dataset")
+    return f'{way_to_dataset}/{name}/{num_of_file:04}.jpg'
+
+
+if __name__ == '__main__':
+    way_to_dataset = os.path.abspath("../python2.1/dataset")
+    name = ['tiger', 'leopard']
+    print("tiger-1 leopard-2")
+    i = int(input())
+    folder_way = way_to_dataset + '/' + name[i]
+    num_of_files = os.listdir(folder_way)
+    for file in range(1, len(num_of_files) + 1):
+        print(get_way(name[0], random.randint(0, len(num_of_files))))
+        file += 1
+
